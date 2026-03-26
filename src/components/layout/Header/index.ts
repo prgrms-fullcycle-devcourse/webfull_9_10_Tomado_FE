7118417fe07b914a6e43962368d6525577b4f919
nogglee <dev.nogglee@gmail.com>
2026-03-26 16:28:36 +0900

    [#52] refactor: Header 컴포넌트 체계 업데이트

--- src/components/layout/Header/index.ts
+++
@@ -1,4 +0,0 @@
-export { Header } from './Header';
-export { DefaultHeader } from './DefaultHeader';
-export { GuestHeader } from './GuestHeader';
-export type { DefaultHeaderProps, GuestHeaderProps, HeaderNavItem, HeaderProps } from './types';
