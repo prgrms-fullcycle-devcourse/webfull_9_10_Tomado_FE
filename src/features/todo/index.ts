135ebd36842284d60552911fd68e8a80ba1a39f2
nogglee <dev.nogglee@gmail.com>
2026-03-27 19:55:23 +0900

    [#31] refactor: todo 관련 로직 재사용 가능한 훅으로 전환 및 todo 도메인에 귀속

--- src/features/todo/index.ts
+++ src/features/todo/index.ts
@@ -2,3 +2,5 @@ export { TodoItem } from './components/TodoItem';
 export type { TodoItemProps } from './components/TodoItem';
 export { TodoInput } from './components/TodoInput';
 export type { TodoInputProps } from './components/TodoInput';
+export { useTodoList, TODO_MAX_CHARS } from './useTodoList';
+export type { Todo } from './useTodoList';
