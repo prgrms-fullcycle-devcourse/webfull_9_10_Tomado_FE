e3c471497f2f82e732a32c9dae5615eb3c3d4420
nogglee <dev.nogglee@gmail.com>
2026-03-28 23:07:00 +0900

    [#30] chore: 전역 타이머 실행기 TimerTicker 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import AppRouter from '@/router';
 import { ToastProvider } from '@/hooks';
+import { TimerTicker } from '@@@/timer';
 import './styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <ToastProvider>
+            <TimerTicker />
             <AppRouter />
         </ToastProvider>
     </StrictMode>
