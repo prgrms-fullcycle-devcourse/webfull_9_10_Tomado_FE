53d86e1125db7969318aec0a7c46a4545a4e13be
nogglee <dev.nogglee@gmail.com>
2026-03-31 19:19:37 +0900

    feat: 인증 여부에 따른 라우팅 분기 처리

--- src/router/PublicOnlyRoute.tsx
+++ src/router/PublicOnlyRoute.tsx
@@ -1,12 +1,14 @@
 import { Navigate } from 'react-router-dom';
 
-import { isAuthenticated } from './auth';
+import { useAuthStore } from '@@@/auth';
 
 type PublicOnlyRouteProps = {
     children: React.ReactNode;
 };
 
 export default function PublicOnlyRoute({ children }: PublicOnlyRouteProps) {
+    const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+
     if (isAuthenticated) {
         return <Navigate to='/main' replace />;
     }
