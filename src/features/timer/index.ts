99bc8de31fd0652cc0ba43bd3e0fc2d7bee1c178
nogglee <dev.nogglee@gmail.com>
2026-04-07 00:10:07 +0900

    chore: 타이머 feature export 구조를 view/controller 분리 기준으로 재정리

--- src/features/timer/index.ts
+++ src/features/timer/index.ts
@@ -5,11 +5,14 @@ export { TimerProgressBar } from './components/TimerProgressBar';
 export { FocusMode } from './components/FocusMode';
 export { FocusModeBackgroundLayer } from './components/FocusModeBackgroundLayer';
 export { TimerTicker } from './components/TimerTicker';
+export { getInitialSeconds } from './selectors';
 
 export { useFocusModeBackground, focusModeBackgrounds } from './useFocusModeBackground';
 export { useTimerMetadata } from './useTimerMetadata';
 export { useTimerNotifications } from './useTimerNotifications';
-export { useTimerSession } from './useTimerSession';
-
+export { useTimerSession } from './useTimerSessionView';
+export { useTimerSessionController } from './useTimerSessionController';
 export { useFocusModeBackgroundStore } from './useFocusModeStore';
 export { useTimerStore } from './useTimerStore';
+
+export type * from './types';
